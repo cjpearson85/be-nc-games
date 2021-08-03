1aa2711f465d3c90749819d07f5a0f47baab3edd
Christopher Pearson <cjpearson@hotmail.com>
2021-08-03 14:51:54 +0100

    implement error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { handle404s, handlePsqlErrors, handleCustomErrors, handle500s } = require('./errors');
 const apiRouter = require('./routers/api.router');
 const app = express();
 
@@ -6,8 +7,9 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
-app.use((err, req, res, next) => {
-
-})
+app.use(handle404s);
+app.use(handlePsqlErrors);
+app.use(handleCustomErrors);
+app.use(handle500s);
 
 module.exports = app;
---
+++ errors.js
@@ -0,0 +1,24 @@
+exports.handle404s = (req, res, next) => {
+  res.status(404).send({ message: "Invalid path" });
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Invalid review_id" });
+  }
+  next(err);
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  const { status, message } = err;
+  if (status) {
+    res.status(status).send({ message });
+  } else {
+    next(err);
+  }
+};
+
+exports.handle500s = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Server side error" });
+};
