ff59f41a23dead6cc27b2028102d18703ef92b71
Christopher Pearson <cjpearson@hotmail.com>
2021-08-06 19:25:11 +0100

    create valuesFormatter function

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,40 +1,19 @@
-const comments = require("../data/development-data/comments");
-
-// extract any functions you are using to manipulate your data, into this file
-exports.createRef = (arr, key1, key2) => {
+exports.createRef = (arr, key, val) => {
     const refObj = {};
     arr.forEach((el) => {
-      refObj[el[key1]] = el[key2];
+      refObj[el[key]] = el[val];
     });
     return refObj;
-  };
-
-exports.categoriesFormatter = (categoryData) => {
-  return categoryData.map((category) => {
-    return [category.slug, category.description];
-  });
-};
-
-exports.usersFormatter = (userData) => {
-  return userData.map((user) => {
-    return [user.username, user.avatar_url, user.name];
-  });
-};
-
-exports.reviewsFormatter = (reviewData) => {
-  return reviewData.map((review) => {
-    return [review.title, review.review_body, review.designer, review.review_img_url, review.votes, review.category, review.owner, review.created_at];
-  });
 };
 
 exports.commentsFormatter = (commentData, refObj) => {
     return commentData.map(comment => {
         return [comment.created_by, refObj[comment.belongs_to], comment.votes, comment.created_at, comment.body]
     })
-}
+};
 
 exports.valuesFormatter = (data, columns) => {
   return data.map(object => {
     return columns.map(column => object[column])
   })
-}
+};
