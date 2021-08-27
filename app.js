1820debf4c2ee05014c8326e7532e7cacc1fed4f
Christopher Pearson <cjpearson@hotmail.com>
2021-08-27 11:51:34 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { handle404s, handlePsqlErrors, handleCustomErrors, handle500s } = require('./errors');
 const apiRouter = require('./routers/api.router');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
