65da077d8b3312e169416b9f821cf185e368a7fb
Christopher Pearson <cjpearson@hotmail.com>
2021-11-24 18:53:06 +0000

    add passwords to user dev data

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,35 +1,43 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
+    username: "tickle122",
+    name: "Tom Tickle",
+    password: "secure123",
     avatar_url:
-      'https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-tickle.svg?v=581547693959934176'
+      "https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-tickle.svg?v=581547693959934176",
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'
+    username: "grumpy19",
+    name: "Paul Grump",
+    password: "secure123",
+    avatar_url: "https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg",
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
+    username: "happyamy2016",
+    name: "Amy Happy",
+    password: "secure123",
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-messy.svg?v=16133629632717342549'
-  },
-  {
-    username: 'weegembump',
-    name: 'Gemma Bump',
-    avatar_url: 'https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-bump.svg?v=12730736646239031661'
-  },
-  {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
+    username: "cooljmessy",
+    name: "Peter Messy",
+    password: "secure123",
     avatar_url:
-      'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'
-  }
+      "https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-messy.svg?v=16133629632717342549",
+  },
+  {
+    username: "weegembump",
+    name: "Gemma Bump",
+    password: "secure123",
+    avatar_url:
+      "https://cdn.shopify.com/s/files/1/0194/0815/8819/t/7/assets/svg--character--mr-bump.svg?v=12730736646239031661",
+  },
+  {
+    username: "jessjelly",
+    name: "Jess Jelly",
+    password: "secure123",
+    avatar_url:
+      "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg",
+  },
 ];
